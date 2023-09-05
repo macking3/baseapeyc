dd4a08143ae3ff2d4fbf865129bedeb66400fcb1
Kayyous1997 <122192830+Kayyous1997@users.noreply.github.com>
2023-09-05 22:56:22 +0100

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from "./Home";
-export { default as Mint } from "./Mint";
+export { default as Mint } from "./Mint-page";
 export { default as Dashboard } from "./Dashboard";
